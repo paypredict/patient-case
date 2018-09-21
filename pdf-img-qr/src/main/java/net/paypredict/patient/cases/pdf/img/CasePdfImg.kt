b9375f4c70d18a4235b010ec4670a1775270c977
Alexei Vylegzhanin <alexei.vylegzhanin@gmail.com>
2018-09-22 02:45:35 +0500

    add pdf-img-qr module

---
+++ pdf-img-qr/src/main/java/net/paypredict/patient/cases/pdf/img/CasePdfImg.kt
@@ -0,0 +1,7 @@
+package net.paypredict.patient.cases.pdf.img
+
+/**
+ * <p>
+ * Created by alexei.vylegzhanin@gmail.com on 9/22/2018.
+ */
+fun extractImages() {}
---
+++ pdf-img-qr/src/test/java/net/paypredict/patient/cases/pdf/img/SaveImagesInPdf.kt
@@ -0,0 +1,58 @@
+package net.paypredict.patient.cases.pdf.img
+
+import org.apache.pdfbox.contentstream.PDFStreamEngine
+import org.apache.pdfbox.contentstream.operator.Operator
+import org.apache.pdfbox.cos.COSBase
+import org.apache.pdfbox.cos.COSName
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject
+import javax.imageio.ImageIO
+import java.io.File
+
+class SaveImagesInPdf(val outDir: File, val name: String) : PDFStreamEngine() {
+    var imageNumber = 1
+    override fun processOperator(operator: Operator, operands: List<COSBase>) {
+        if ("Do" == operator.name) {
+            val objectName = operands[0] as COSName
+            val xObject = resources.getXObject(objectName)
+            when (xObject) {
+                is PDImageXObject -> {
+                    ImageIO.write(
+                        xObject.image,
+                        "PNG",
+                        outDir.resolve("$name.image_$imageNumber.png")
+                    )
+                    println("Image saved.")
+                    imageNumber++
+
+                }
+                is PDFormXObject -> showForm(xObject)
+            }
+        } else {
+            super.processOperator(operator, operands)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val fileName = args.first()
+            var document: PDDocument? = null
+            try {
+                val file = File(fileName)
+                document = PDDocument.load(file)
+                val printer = SaveImagesInPdf(file.parentFile, file.name)
+                var pageNum = 0
+                for (page in document.pages) {
+                    pageNum++
+                    println("Processing page: $pageNum")
+                    printer.processPage(page)
+                }
+            } finally {
+                document?.close()
+            }
+        }
+    }
+
+}
