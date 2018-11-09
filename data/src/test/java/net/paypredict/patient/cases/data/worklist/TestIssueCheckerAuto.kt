9ea737f3a9ea2f1bdfe00704a9b84fa109bd43e0
Alexei Vylegzhanin <alexei.vylegzhanin@gmail.com>
2018-11-10 01:58:34 +0500

    remove old logic in TestIssueCheckerAuto.kt

--- data/src/test/java/net/paypredict/patient/cases/data/worklist/TestIssueCheckerAuto.kt
+++ data/src/test/java/net/paypredict/patient/cases/data/worklist/TestIssueCheckerAuto.kt
@@ -2,8 +2,6 @@ package net.paypredict.patient.cases.data.worklist
 
 import com.mongodb.client.MongoCollection
 import net.paypredict.patient.cases.mongo.DBS.Collections.cases
-import net.paypredict.patient.cases.mongo._id
-import net.paypredict.patient.cases.mongo.`$set`
 import net.paypredict.patient.cases.mongo.doc
 import org.bson.Document
 
@@ -15,15 +13,6 @@ object TestIssueCheckerAuto {
     fun main(args: Array<String>) {
         val cases: MongoCollection<Document> = cases()
         val case = cases.find(doc { doc["case.Case.accessionNumber"] = args[0] }).first()
-
-        val _id = case.getString("_id")!!._id()
-        cases.updateOne(_id, doc {
-            doc[`$set`] = doc {
-                doc["status.problems"] = 0
-                doc["status.value"] = null
-            }
-        })
-        cases.deleteOne(_id)
 
         val issueCheckerAuto =
             IssueCheckerAuto(
