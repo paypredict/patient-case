bf5b087f31509eefed6d4dd2840e159709d8bd58
Alexei Vylegzhanin <alexei.vylegzhanin@gmail.com>
2018-12-11 02:23:36 +0500

    fix refreshing of grid on update case data

--- web/src/main/java/net/paypredict/patient/cases/view/CaseAttrGrid.kt
+++ web/src/main/java/net/paypredict/patient/cases/view/CaseAttrGrid.kt
@@ -5,7 +5,7 @@ import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.icon.Icon
 import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.component.orderedlayout.ThemableLayout
-import com.vaadin.flow.data.provider.DataProvider
+import com.vaadin.flow.data.provider.CallbackDataProvider
 import com.vaadin.flow.data.provider.Query
 import com.vaadin.flow.data.provider.QuerySortOrder
 import com.vaadin.flow.data.provider.SortDirection
@@ -199,20 +199,22 @@ class CaseAttrGrid : Composite<Grid<CaseAttr>>(), ThemableLayout {
     }
 
     fun refresh() {
-        content.dataProvider = DataProvider.fromFilteringCallbacks(
-            { query: Query<CaseAttr, Unit> ->
-                collection()
-                    .find(filter)
-                    .projection(projection)
-                    .sort(query.toMongoSort())
-                    .skip(query.offset)
-                    .limit(query.limit)
-                    .map { it.toCaseAttr() }
-                    .toList()
-                    .stream()
-            },
-            { collection().count(filter).toInt() }
-        )
+        content.dataProvider =
+                CallbackDataProvider(
+                    { query: Query<CaseAttr, Unit> ->
+                        collection()
+                            .find(filter)
+                            .projection(projection)
+                            .sort(query.toMongoSort())
+                            .skip(query.offset)
+                            .limit(query.limit)
+                            .map { it.toCaseAttr() }
+                            .toList()
+                            .stream()
+                    },
+                    { collection().count(filter).toInt() },
+                    { source: CaseAttr? -> source?._id }
+                )
     }
 
 
